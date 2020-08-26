1a45f565a3d09b66d8b84ed0d54a737d6e9aa567
Vinicius Henrique <Vinicius-almeida01@hotmail.com>
2020-08-26 17:50:12 -0300

    Tela inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title> Sequência Fibonacci (FibonaThi :3) </title>
+	</head>
+	<body>
+		<form action="processa_dados.php" method="POST">
+			<label> Informe um numero </label>
+			<input type="number" name="infnum"/>
+            
+			<input type="submit" value="Enviar"/>
+		</form>
+	</body>
+</html>
