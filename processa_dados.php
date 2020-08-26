f9b6c8e3899d7424b0b53115aa96a6fb64bcc39e
Vinicius Henrique <Vinicius-almeida01@hotmail.com>
2020-08-26 18:03:19 -0300

    arrumando um erro

--- processa_dados.php
+++ processa_dados.php
@@ -1,19 +1,19 @@
 <?php 
     $numInf = $_POST["infnum"];
-    $ulti = 1;
-    $penul = 0;
+    $antecessor = 1;
+    $atual = 0;
         
     echo "0<br/>
             1<br/>"; 
 
     for($con=1 ; $con<=$numInf-2 ; $con++)
     {
-        $new = $ulti + $penul;
+        $new = $antecessor + $atual;
         echo $new."<br/>";
        
         
-        $penul = $ulti;
-        $ulti = $new;
+        $atual = $antecessor;
+        $antecessor = $new;
     } 
 
     echo "<br/>A sequência de fibonacci nada mais é que a soma de um numero mais o seu anterior, exemplo, 3 +5 = 8. PS: Sempre começa do 0 e repete 2 vezes o 1";
